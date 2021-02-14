1c6f06f81446ad258290396c19c18ee740b518ad
Aissous Amin <amin.aissous.pro@gmail.com>
2021-02-14 16:57:19 +0100

    Add responseController to index file

--- Source/Infrastructure/index.js
+++ Source/Infrastructure/index.js
@@ -1,5 +1,7 @@
 const ExpressServer = require('./ExpressServer');
+const ResponseController = require('./ExpressResponseController');
 
 module.exports = {
     ExpressServer,
+    ResponseController,
 };
