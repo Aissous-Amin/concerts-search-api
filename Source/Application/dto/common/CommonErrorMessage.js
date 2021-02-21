5e0f32ade954f67c3d3fe5c3a831e5d32ca6a5bf
Aissous Amin <amin.aissous.pro@gmail.com>
2021-02-21 15:16:34 +0100

    Add ComonErrorMessage DTO

--- Source/Application/dto/common/CommonErrorMessage.js
+++ Source/Application/dto/common/CommonErrorMessage.js
@@ -4,36 +4,36 @@ class CommonErrorMessage {
     static validate(errors) {
         errors.forEach((err) => {
             switch (err.local.key) {
-                case 'bandIds':
-                    err.message = {
-                        message: functionalMessage.concerts.BANDIDS.MESSAGE,
-                        id: functionalMessage.concerts.BANDIDS.ID,
-                        type: functionalMessage.concerts.BANDIDS.TYPE,
-                    };
-                    break;
-                case 'latitude':
-                    err.message = {
-                        message: functionalMessage.concerts.LATITUDE.MESSAGE,
-                        id: functionalMessage.concerts.LATITUDE.ID,
-                        type: functionalMessage.concerts.LATITUDE.TYPE,
-                    };
-                    break;
-                case 'longitude':
-                    err.message = {
-                        message: functionalMessage.concerts.LONGITUDE.MESSAGE,
-                        id: functionalMessage.concerts.LONGITUDE.ID,
-                        type: functionalMessage.concerts.LONGITUDE.TYPE,
-                    };
-                    break;
-                case 'radius':
-                    err.message = {
-                        message: functionalMessage.concerts.RADIUS.MESSAGE,
-                        id: functionalMessage.concerts.RADIUS.ID,
-                        type: functionalMessage.concerts.RADIUS.TYPE,
-                    };
-                    break;
-                default:
-                    break;
+            case 'bandIds':
+                err.message = {
+                    message: functionalMessage.concerts.BANDIDS.MESSAGE,
+                    id: functionalMessage.concerts.BANDIDS.ID,
+                    type: functionalMessage.concerts.BANDIDS.TYPE,
+                };
+                break;
+            case 'latitude':
+                err.message = {
+                    message: functionalMessage.concerts.LATITUDE.MESSAGE,
+                    id: functionalMessage.concerts.LATITUDE.ID,
+                    type: functionalMessage.concerts.LATITUDE.TYPE,
+                };
+                break;
+            case 'longitude':
+                err.message = {
+                    message: functionalMessage.concerts.LONGITUDE.MESSAGE,
+                    id: functionalMessage.concerts.LONGITUDE.ID,
+                    type: functionalMessage.concerts.LONGITUDE.TYPE,
+                };
+                break;
+            case 'radius':
+                err.message = {
+                    message: functionalMessage.concerts.RADIUS.MESSAGE,
+                    id: functionalMessage.concerts.RADIUS.ID,
+                    type: functionalMessage.concerts.RADIUS.TYPE,
+                };
+                break;
+            default:
+                break;
             }
         });
         return errors;
