9c7522b6c6fd6273580a421e7019a5efc7d13a59
Aissous Amin <amin.aissous.pro@gmail.com>
2021-02-25 10:21:17 +0100

    Refactoring code source

--- Config/lib/database.js
+++ Config/lib/database.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 const env = require('../env/variables');
-const fs = require('fs')
+const fs = require('fs');
 
 
 let pool = null;
@@ -15,14 +15,14 @@ let pool = null;
 
 class MONGOOSE {
     /**
+     * Init Mongoose Function.
      *
      * @param parameters
+     * @param {object} parameters.
      * @property {string} parameters.server - Database machine (env var : DATABASE_SERVER).
      * @property {string} parameters.user - Database user (en var :DATABASE_USER).
      * @property {string} parameters.password - Database password (env var :DATABASE_PASSWORD).
-     *
      * @returns {Promise<void>}
-     *
      */
     static async init(parameters = {
         server: env.DATABASE_SERVER || '',
@@ -39,7 +39,7 @@ class MONGOOSE {
             });
             /** Set up default mongoose connection. */
             await mongoose.connect(extendedParameters.server, extendedParameters);
-            pool =  mongoose.connection;
+            pool = mongoose.connection;
             pool.on('error', console.error.bind(console, 'MongoDB connection error:'));
         } catch (e) {
             /** Bind connection to error event (to get notification of connection errors). */
@@ -68,16 +68,14 @@ class MONGOOSE {
     static async import_data(data) {
         try {
             const promises = [];
-            const concerts = JSON.parse(fs.readFileSync(__dirname + '/data/concerts.json', 'utf-8'));
-            const bands = JSON.parse(fs.readFileSync(__dirname + '/data/bands.json', 'utf-8'));
-            const venues = JSON.parse(fs.readFileSync(__dirname + '/data/venues.json', 'utf-8'));
-            // Temporary Solution : The best way to apply change in data it's to use post hooks functions .pre
+            const concerts = JSON.parse(fs.readFileSync(`${__dirname}/data/concerts.json`, 'utf-8'));
+            const bands = JSON.parse(fs.readFileSync(`${__dirname}/data/bands.json`, 'utf-8'));
+            const venues = JSON.parse(fs.readFileSync(`${__dirname}/data/venues.json`, 'utf-8'));
             promises.push(data.Concerts.insertMany(concerts));
             promises.push(data.Bands.insertMany(bands));
             promises.push(data.Venues.insertMany(venues));
             console.log('Importing data to mongodb database...');
             await Promise.all(promises);
-            // pool.model.createIndex({"location": "2dsphere"})
             console.log('Import data to mongodb database : Done!');
         } catch (e) {
             console.error(e.message);
